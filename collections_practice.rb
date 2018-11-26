10fb9ba81649e5c3ad3fea673e2579cd96b27827
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-26 04:02:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
-# your code goes here
+def begins_with_r(tools)
+  tools.each do |item|
+    if item[0] != "r"
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(elements)
+  elements.select { |x| x.include?(x) }
+end
